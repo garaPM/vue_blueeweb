a27dfb5249ebe38bb0824c7df7884cbc0e5d89dd
garaPM <gonzalo.ruiz.anabalon@gmail.com>
2019-06-30 22:50:59 -0400

    clase 6 propiedades observadores

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const app = new Vue({
   el: '#app',
   data: {
-    fondo: 'bg-warning',
-    color: false
-
+    mensaje: 'Hola probando los propiedades computadas',
+    contador: 0
   },
   methods: {
 
   },
   computed: {
+    invertido() {
+      return this.mensaje.split('').reverse().join('');
+    },
+    color() {
+      return {
+        'bg-success': this.contador <= 10,
+        'bg-warning': this.contador > 10 && this.contador < 20,
+        'bg-danger': this.contador >= 20
+      }
+    }
 
   }
 });
